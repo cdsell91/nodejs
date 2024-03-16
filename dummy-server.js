1644d2eb1735a2ff6b66b7a04fad5a166fa356cf
cesar <cesar@c.ubuntu>
2024-03-16 18:02:51 -0300

    dummy-server

--- dummy-server.js
+++ dummy-server.js
@@ -11,18 +11,13 @@ app.use(bodyParser.json());
 
 app.post('/login', (req, res) => { 
     const { username, password } = req.body;
-    if (username === 'user' && password === 'password') {
-    
-        res.status(200).json({
+    res.status(200).json({
         token: 'token_de_autenticacion_generado',
         user: {
             username: username,
             email: 'usuario@example.com',
         },
-        });
-    } else {
-        res.status(401).json({ error: 'Credenciales incorrectas' });
-    }
+    });
 });
 
 
